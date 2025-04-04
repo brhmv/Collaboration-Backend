299113b0e2faf5ce8a80a723bef92f6c9d577d4a
anarouis <anarftliyev@gmail.com>
2025-04-04 14:47:10 +0400

    initial commit

---
+++ src/test/java/az/edu/turing/turingcollab/TuringCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.turingcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringCollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
